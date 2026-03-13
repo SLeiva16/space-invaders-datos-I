482e16aa98e2fd14043b73eb95fe6d7fc30cf7de
Leiva <seleiva16@gmail.com>
2026-03-13 16:48:46 -0600

    Prueba para mandar a GitHup

---
+++ src/server/GameServer.java
@@ -0,0 +1,29 @@
+package server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GameServer {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)){
+            System.out.println("Space Invaders Game");
+            System.out.println("El server inicio en el puerto: " + PORT);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("[Server] Se conecto: " +
+                        clientSocket.getInetAddress().getHostName());
+
+            }
+        } catch (Exception e) {
+            System.out.println("[Server] Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+
+    }
+}
