f5ff95c9fcc60cce71bb4510daa291d52475c078
Leiva <seleiva16@gmail.com>
2026-03-13 19:26:27 -0600

    - Se añadieron modulos de cliente y servidor. - Se implemento la interfaz de usuario de la pantalla de inicio de sesion con JavaFX. - Se creo un servidor con ServerSocket con el puerto 12345. - Se preparó la estructura para futuras funciones de red (control de cliente, emparejamiento, etc.).

---
+++ src/client/GameClient.java
@@ -0,0 +1,18 @@
+package client;
+
+import client.screens.LoginScreen;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameClient extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Space Invaders Game");
+        primaryStage.setResizable(false);
+
+        LoginScreen loginScreen = new LoginScreen(primaryStage);
+        loginScreen.show();
+
+    }
+    public static void main(String[] args) { launch(args); }
+}
---
+++ src/client/screens/LoginScreen.java
@@ -0,0 +1,105 @@
+package client.screens;
+
+
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class LoginScreen {
+
+    private static final int PORT = 12345 ;
+    private final Stage stage;
+
+
+    private TextField ipField;
+    private TextField usernameField;
+    private PasswordField passwordField;
+    private Label statusLabel;
+
+    public LoginScreen(Stage stage) {this.stage = stage;}
+
+    public void show() {
+        VBox root = new VBox(20);
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(45));
+        root.setStyle("-fx-background-color: black");
+
+        Label title = new Label("Space Invader");
+        title.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+        title.setTextFill(Color.GREEN);
+
+        Label subtitle = new Label("Conectado al Serveer");
+        subtitle.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+        subtitle.setTextFill(Color.GRAY);
+
+        ipField        = styledField("Server IP");
+        usernameField  = styledField("Usuario");
+        passwordField  = new PasswordField();
+        styleControl(passwordField, "Contraseña");
+
+        Button loginBtn = styledButton("Iniciar Sesion", "#00ff88" );
+        Button registraseBtn = styledButton("Registrarse", "#00ff88" );
+
+        statusLabel = new Label("");
+        statusLabel.setFont(Font.font("Verdana", FontWeight.NORMAL, 12));
+        statusLabel.setTextFill(Color.GREEN);
+
+        root.getChildren().addAll(
+                title, subtitle,
+                ipField, usernameField, passwordField,
+                loginBtn, registraseBtn,
+                statusLabel);
+
+
+        Scene scene = new Scene(root, 500,500);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /*
+          =====Style=====
+    */
+    private TextField styledField(String prompt) {
+        TextField f = new TextField();
+        styleControl(f, prompt);
+        return f;
+    }
+
+    private void styleControl(Control c, String prompt) {
+        c.setStyle(
+                "-fx-background-color: #161b22;" +
+                        "-fx-text-fill: #c9d1d9;" +
+                        "-fx-border-color: #30363d;" +
+                        "-fx-border-radius: 4;" +
+                        "-fx-background-radius: 4;" +
+                        "-fx-font-family: Verdana;" +
+                        "-fx-font-size: 13;"
+        );
+        c.setPrefWidth(300);
+        if (c instanceof TextField) ((TextField) c).setPromptText(prompt);
+        if (c instanceof PasswordField) ((PasswordField) c).setPromptText(prompt);
+    }
+
+    private Button styledButton(String text, String color) {
+        Button b = new Button(text);
+        b.setPrefWidth(300);
+        b.setFont(Font.font("Monospace", FontWeight.BOLD, 13));
+        b.setStyle(
+                "-fx-background-color: " + color + ";" +
+                        "-fx-text-fill: #0d1117;" +
+                        "-fx-border-radius: 4;" +
+                        "-fx-background-radius: 4;"
+        );
+        return b;
+    }
+
+
+
+}
